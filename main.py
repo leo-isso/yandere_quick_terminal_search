f5731165f727a95b5bdf10d55326f85345f1a256
Leonardo Isso <leoisso.work@gmail.com>
2023-03-24 20:11:58 -0300

    feat: implement initial ui

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from posts import Posts 
+from ui import TerminalUI
 
 if __name__ == '__main__':
-    tags_input = input('Type the tags you\'re looking for: ')
-    posts = Posts(tags=tags_input)
-    print(posts.get())
+    interface = TerminalUI()
+    interface.init()
