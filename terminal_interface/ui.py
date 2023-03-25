c8ad8898b1e1cf24af31d387b6f55bcc2aedc55b
Leonardo Isso <leoisso.work@gmail.com>
2023-03-25 19:40:22 -0300

    feat: full TI refactor

--- terminal_interface/ui.py
+++ terminal_interface/ui.py
@@ -2,7 +2,7 @@ from domains.posts import Posts, PostInterface
 
 class TerminalUI:
     TOPICS = (
-        (1, Posts, PostInterface),
+        (1, Posts, PostInterface()),
         # (2, 'Tags'),
     )
 
@@ -11,23 +11,17 @@ class TerminalUI:
         self.topic = 0
 
     def init(self,):
-        while not self.exit_ui:
-            topic = input('(1)images\n(2)tags\n(0)tags\nSearch for: ')
+        topic = self._get_topic()
 
-            try:
-                self.topic = int(topic)
-                if not self._is_valid_topic():
-                    raise ValueError
-            except ValueError:
-                print('The inputed value is invalid')
-                print('Exiting...')
-                self.exit_ui = True
-                continue
-            
-            self.init_topic_flow()
-            
+        if not self._is_valid_topic(topic):
+            self._send_invalid_message()
+            return self.init()
+        
+        self._set_topic(topic)            
+        self.init_topic_flow()
+
     def init_topic_flow(self):
-        _, topic_actions, topic_interface = self._get_selected_topic()
+        _, topic_actions, topic_interface = self.selected_topic
 
         print(topic_interface.selected_message)
         tags = input(topic_interface.input_message)
@@ -36,17 +30,39 @@ class TerminalUI:
         
         self.exit_ui = True
         
-    def _get_selected_topic(self):
+
+    def _is_valid_topic(self, topic):
+        try:
+            topic = int(topic)
+
+            if topic == 0:
+                return True
+            
+            for _topic in self.TOPICS:
+                if topic in _topic:
+                    return True
+                
+        except ValueError:
+            return False
+        
+    # messages
+    def _send_invalid_message():
+        print('\nThe inserted value is invalid, please try again...\n')
+
+
+    # props, sets, gets
+    @property
+    def selected_topic(self):
         for topic in self.TOPICS:
             if self.topic in topic:
                 return topic
 
-    def _is_valid_topic(self):
-        if self.topic == 0:
-            return False
+    def _get_topic(self):
+        topics = ''.join(
+            [f'\n{index} - {interface.domain_name}' for index, _, interface in self.TOPICS]
+        )
+        return input(f'Select a topic:\n0 - Exit{topics}\n>')
+
+    def _set_topic(self, topic):
+        self.topic = int(topic)
         
-        for topic in self.TOPICS:
-            if self.topic in topic:
-                return True
-            
-        return False
