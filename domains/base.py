e719061fc63a65e08dfefa499bbe2d9364fb3e0a
Leonardo Isso <leoisso.work@gmail.com>
2023-03-26 15:37:15 -0300

    feat: improve domains abstraction

--- domains/base.py
+++ domains/base.py
@@ -3,3 +3,17 @@ import requests
 class BaseDomain:
     base_url = 'https://yande.re/'
     requests = requests
+
+    @property
+    def ENTITY_URL(self):
+        return NotImplementedError()
+
+    def get(self, **kwargs):
+        params = dict(**kwargs)
+
+        request = self.requests.get(
+            url=self.ENTITY_URL,
+            params=params,
+        )
+
+        return request.json()
--- domains/posts.py
+++ domains/posts.py
@@ -1,23 +1,7 @@
 from domains.base import BaseDomain
-from utils import split_tags
 
 class Posts(BaseDomain):
-    def __init__(self, tags):
-        super().__init__()
-        self.ENTITY_URL = f'{self.base_url}post.json'
-        self.tags = tags
-        self.page = 0
-
-    def get(self):
-        params = dict(
-            tags=split_tags(self.tags),
-            page=self.page
-        )
-
-        request = self.requests.get(
-            url=self.ENTITY_URL,
-            params=params,
-        )
-
-        return request.json()
+    @property
+    def ENTITY_URL(self):
+        return f'{self.base_url}post.json'
     
